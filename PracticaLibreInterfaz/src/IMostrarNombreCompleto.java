0006f0d60428a82149add5af5f2361f585d5a029
juanjapon <juanjapon@gmail.com>
2023-10-28 09:42:56 -0500

    Interfaz practica libre

---
+++ PracticaLibreInterfaz/src/IMostrarNombreCompleto.java
@@ -0,0 +1,3 @@
+public interface IMostrarNombreCompleto {
+    public void mostrarNombreCompleto(String primerNombre,String segundoNombre,String primerApellido,String segundoApellido);
+}
---
+++ PracticaLibreInterfaz/src/Main.java
@@ -0,0 +1,10 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        IMostrarNombreCompleto nombre=(nombre1,nombre2,apellido1,apellido2)-> System.out.println(nombre1+" "+nombre2+" "+apellido1+" "+apellido2);
+
+        nombre.mostrarNombreCompleto("Juan","Pedro","Mendoza","Ramirez");
+    }
+
+}
